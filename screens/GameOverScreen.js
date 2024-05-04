2d2deb9bd9b370125abcde84dd4edd9ed12d0628
Evan <evanz1215@gmail.com>
2024-05-04 17:34:17 +0800

    feat: Improve GameOverScreen UI and add Start New Game button

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,7 @@
 import { View, Image, StyleSheet, Text } from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function GameOverScreen() {
   return (
@@ -12,9 +13,13 @@ function GameOverScreen() {
           source={require("../assets/images/success.png")}
         />
       </View>
-      <View>
-        <Text>Your phone needed X rounds to guess the number Y.</Text>
-      </View>
+
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number
+        <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <PrimaryButton onPress={() => {}}>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -40,5 +45,15 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
